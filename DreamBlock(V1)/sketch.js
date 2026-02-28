d533fc2bf8b1757f97489d1d180f4af432af4410
MeltyKelpy <mehbeingrandom@gmail.com>
2026-02-28 13:37:37 -0600

    completely broke the project and then rebuilt it
    also phase platforms

--- DreamBlock(V1)/sketch.js
+++ DreamBlock(V1)/sketch.js
@@ -15,6 +15,7 @@ var colors = ["red", "green", "blue", "yellow"]
 var platforms = []
 var phase_platforms = []
 var isOnPlatform = false;
+var camera_constraints = [[200, 100], [300, 100]];
 
 function setup() {
   const game = createCanvas(800,400);
@@ -24,9 +25,12 @@ function setup() {
   player_ig.setCollider("rectangle", 0,0,50,50);
   player_ig.shapeColor = color(34, 56, 76);
 
-  platform = createSprite(400, 400, 1500, 100);
+  var platform = createSprite(400, 400, 1500, 100);
+  var platform3 = createSprite(100, 200, 300, 50);
   platforms.push(platform);
-  platform2 = createSprite(600, 200, 300, 50);
+  platforms.push(platform3);
+
+  var platform2 = createSprite(600, 200, 300, 50);
   phase_platforms.push(platform2);
   
 }
@@ -42,11 +46,10 @@ function draw() {
     player_ig.velocity.y = 0;
     player_ig.velocity.x = 0;
   }
-  
-  // console.log(player_ig.velocity.x + ", " + player_ig.velocity.y);
-  
+
   for (let i = 0; i < platforms.length; i++) {
-    if(player_ig.collide(platforms[i])) {
+    platforms[i].shapeColor = "#2D2F3C";
+    if (player_ig.collide(platforms[i])) {
       let noGo = platforms[i].position.y + (platforms[i].height);
       if (player_ig.position.y <= platforms[i].position.y && player_ig.position.y < noGo) {
         isOnPlatform = true;
@@ -61,9 +64,12 @@ function draw() {
   }
 
   for (let i = 0; i < phase_platforms.length; i++) {
-    if(player_ig.collide(phase_platforms[i])) {
-      if (player_ig.position.y <= phase_platforms[i].position.y) {
-        isOnPlatform = true;
+    phase_platforms[i].shapeColor = "#7C4323";
+    // why does +20 and -5 work? I DONT KNOW!!!!!
+    if (player_ig.position.y+(player_ig.height-5) <= (phase_platforms[i].position.y+20)) {
+      isOnPlatform = true;
+      phase_platforms[i].physicsType = 'DYNAMIC';
+      if (player_ig.collide(phase_platforms[i])) {
         if (dashFrames <= 0) {
           player_ig.velocity.y = 0;
           player_ig.velocity.x = 0;
@@ -93,7 +99,17 @@ function draw() {
   }
 
   isOnPlatform = false;
-
+  
+  if((keyWentDown("z") || keyWentDown("space"))){
+    if (CANJUMP) {
+      player_ig.velocity.y = -JUMP;
+      CANJUMP = false;
+    }
+    else {
+      storeJump = 10;
+    }
+  }
+  
   if (dashFrames <= 0) {
     player_ig.velocity.x = 0;
     if ((keyIsDown("68") || keyIsDown("39")) && CANJUMP) {
@@ -115,27 +131,17 @@ function draw() {
       }
     }
   }
-  
-  if((keyWentDown("z") || keyWentDown("space"))){
-    if (CANJUMP) {
-      player_ig.velocity.y = -JUMP;
-      CANJUMP = false;
-    }
-    else {
-      storeJump = 10;
-    }
-  }
-  
+
   if (dasher == true || dashFrames >= 11) {
       CANDASH = false;
       dasher = false;
       let dashers = 0;
       let jumpAfter = true;
+      player_ig.velocity.x = 0;
+      player_ig.velocity.y = 0;
       if (dashFrames <= 0) {
         dashFrames = 12;
       }
-      player_ig.velocity.y = 0;
-      player_ig.velocity.x = 0;
       if (keyIsDown("65") || keyIsDown("37")) {
         dashers += 1;
         jumpAfter = false;
@@ -146,9 +152,6 @@ function draw() {
         jumpAfter = false;
         player_ig.velocity.x = 9;
       }
-
-      // JANK ASS CODE BELOW
-
       if (keyIsDown("87") || keyIsDown("38")) {
         dashers += 1;
         jumpAfter = false;
@@ -178,13 +181,32 @@ function draw() {
   if (keyWentDown("e")) {
     current_color += 1;
   }
-  
   if (current_color >= colors.length) {
     current_color = 0;
   }
   if (current_color < 0) {
     current_color = colors.length-1;
   }
+
+
+  camera.zoom = 0.5;
+  
+  camera.position.x = lerp(camera.position.x, player_ig.position.x+(100), 0.2);
+  if (camera.position.x <= camera_constraints[0][0]) {
+    camera.position.x = camera_constraints[0][0];
+  }
+  else if (camera.position.x >= camera_constraints[1][0]) {
+    camera.position.x = camera_constraints[1][0];
+  }
+
+  camera.position.y = lerp(camera.position.y, player_ig.position.y-(25), 0.2);
+  if (camera.position.y <= camera_constraints[0][1]) {
+    camera.position.y = camera_constraints[0][1];
+  }
+  else if (camera.position.y >= camera_constraints[1][1]) {
+    camera.position.y = camera_constraints[1][1];
+  }
   
   drawSprites();
+
 }
