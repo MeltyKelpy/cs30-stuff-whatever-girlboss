870470d1af5a79706cfee06d13241d0b5f7f9778
MeltyKelpy <91714683+MeltyKelpy@users.noreply.github.com>
2026-03-09 11:03:30 -0600

    the square is real

--- 2DBattlegrounds/sketch.js
+++ 2DBattlegrounds/sketch.js
@@ -2,6 +2,8 @@
 
 let my, guests, shared;
 let spawn_cords = {x: 500, y: 100}
+let platforms = [];
+let buddy;
 
 function preload() {
   // connect to a p5party server
@@ -9,82 +11,38 @@ function preload() {
   my = partyLoadMyShared();
   guests = partyLoadGuestShareds();
   shared = partyLoadShared("shared", {
-    platforms : [],
+    player_sprites : [],
   });
-}
-
-function createPlatform(x = 0, y = 0, width = 10, height = 10) {
-  var platform = createSprite(x,y,width,height);
-  platform.shapeColor = color(255);
-  shared.platforms.push(platform);
 }
 
 function setup() {
   createCanvas(5000, 720);
 
   my.me = new pal(spawn_cords.x, spawn_cords.y);
+  console.log("am i host?", partyIsHost());
 
   createPlatform(150,700,width,50);
 }
 
+function createPlatform(x = 0, y = 0, width = 10, height = 10) {
+  let platform = createSprite(x,y,width,height);
+  platform.shapeColor = color(255);
+  platforms.push(platform);
+}
+
+
 function draw() {
   background(50);
 
-  collisionManagement();
-  cameraFunctions();
-  playerMover();
+  my.me.collisionManager();
+  my.me.cameraFunctions();
+  my.me.update();
+
+  for (let i = 0; i < shared.player_sprites.length; i++) {
+    rect(shared.player_sprites[i].x,shared.player_sprites[i].x,shared.player_sprites[i].width,shared.player_sprites[i].height);
+  }
 
   drawSprites();
-}
-
-function playerMover() {
-  my.me.guy.velocity.x = 0;
-  if ((keyIsDown("68") || keyIsDown("39")) && CANJUMP) {
-    my.me.guy.velocity.x += SPEED;
-  }
-  if ((keyIsDown("65") || keyIsDown("37")) && CANJUMP) {
-    my.me.guy.velocity.x -= SPEED;
-  }
-  if ((keyIsDown("68") || keyIsDown("39")) && !CANJUMP) {
-    my.me.guy.velocity.x += SPEED;
-  }
-  if ((keyIsDown("65") || keyIsDown("37")) && !CANJUMP) {
-    my.me.guy.velocity.x -= SPEED;
-  }
-}
-
-function collisionManagement() {
-  // taken from dream block <3
-  // why in the hell would i code it again
-
-  if (my.me.isOnPlatform) {
-    my.me.guy.velocity.y = 0;
-    my.me.guy.velocity.x = 0;
-  }
-
-  for (let i = 0; i < shared.platforms.length; i++) {
-    if (my.me.guy.collide(shared.platforms[i])) {
-      let noGo = shared.platforms[i].position.y + (shared.platforms[i].height);
-      if (my.me.y <= shared.platforms[i].position.y && my.me.y < noGo) {
-        my.me.guy.velocity.y = 0;
-        my.me.guy.velocity.x = 0;
-        my.me.canJump = true;
-      }
-    }
-  }
-}
-
-function cameraFunctions() {
-  camera.zoom = 1.1;
-  
-  camera.position.x = lerp(camera.position.x, my.me.x, 0.1);
-  if (camera.position.x <= 100) {
-    camera.position.x = 100;
-  }
-  else if (camera.position.x >= 1000) {
-    camera.position.x = 1000;
-  }
-
 }
 
   // camera.position.y = lerp(camera.position.y, my.me.y, 0.1);
@@ -95,16 +53,66 @@ function cameraFunctions() {
   //   camera.position.y = camera_constraints[1][1];
   // }
 
+
 class pal {
-  constructor(x = 0, y = 0, dir = 0) {
+  constructor(x = 0, y = 0) {
     this.canJump = true;
     this.isOnPlatform = false;
-    this.guy = createSprite(x, y, 50, 50);
-    this.guy.setCollider("rectangle", 0,0,50,50);
-    this.guy.shapeColor = color(34, 56, 76);
+    this.speed = 10;
+    shared.player_sprites.push({x: x, y: y, width: 50, height: 50});
+    this.velocity = {x: 0, y: 0};
   }
 
   update() {
-    console.log("hi i exist");
+    this.position += this.velocity;
+
+    this.velocity.x = 0;
+    if ((keyIsDown("68") || keyIsDown("39")) && this.canJump) {
+      this.velocity.x += this.speed;
+    }
+    if ((keyIsDown("65") || keyIsDown("37")) && this.canJump) {
+      this.velocity.x -= this.speed;
+    }
+    if ((keyIsDown("68") || keyIsDown("39")) && !this.canJump) {
+      this.velocity.x += this.speed;
+    }
+    if ((keyIsDown("65") || keyIsDown("37")) && !this.canJump) {
+      this.velocity.x -= this.speed;
+    }
   }
+    
+  collisionManager() {
+  
+    if (this.isOnPlatform) {
+      this.velocity.y = 0;
+      this.velocity.x = 0;
+    }
+    else {
+      this.velocity.y += 10;
+    }
+  
+    // for (let i = 0; i < platforms.length; i++) {
+    //   if (this.collide(platforms[i])) {
+    //     let noGo = platforms[i].position.y + (platforms[i].height);
+    //     if (this.position.y <= platforms[i].position.y && my.me.guy.position.y < noGo) {
+    //       this.velocity.y = 0;
+    //       this.velocity.x = 0;
+    //       this.canJump = true;
+    //     }
+    //   }
+    // }
+  }
+  
+  cameraFunctions() {
+    camera.zoom = 1.1;
+    
+    camera.position.x = lerp(camera.position.x, my.me.x, 0.1);
+    if (camera.position.x <= 100) {
+      camera.position.x = 100;
+    }
+    else if (camera.position.x >= 1000) {
+      camera.position.x = 1000;
+    }
+  }
+
 }
